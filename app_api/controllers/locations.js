eefe91f2b19141a075d7994d1d581a8a00b9256f
michaelbotes <michael@ourhood.co.za>
2016-11-21 13:10:22 +0200

    -database added to online Mongolab

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -12,18 +12,33 @@ var sendJsonResponse = function(res, status, content) {
 module.exports.locationsCreate = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.locationsListByDistance = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.locationReadOne = function (req, res) {
-    Loc.findById(req.params.locationid)
-        .exec(function(err, location){
-           sendJsonResponse(res, 200, location);
-        });
+    if (req.params && req.params.locationid) {
+        Loc.findById(req.params.locationid)
+            .exec(function(err, location){
+                if (!location) {
+                    sendJsonResponse(res, 404, {"message": "locationid not found"});
+                    return;
+                } else if (err) {
+                    sendJsonResponse(res, 404, err);
+                    return;
+                }
+                sendJsonResponse(res, 200, location);
+            });
+    } else {
+        sendJsonResponse(res, 404, {"message": "No locationid in request"});
+    }
 };
+
 module.exports.locationsUpdateOne = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
+
 module.exports.locationDeleteOne = function (req, res) {
     sendJsonResponse(res, 200, {"status" : "success"});
 };
